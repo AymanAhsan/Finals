eef0524b69d4ea19d1aa3aac80795aa237ce7a34
AymanAhsan <aymanahsan06@gmail.com>
2025-05-18 01:11:24 -0400

    real

--- Finals/main.cpp
+++ Finals/main.cpp
@@ -6,20 +6,9 @@
 
 
 int main() {
-	Player me;
-	Character rival;
-	Pokemon* turtwig = new Pokemon("Turtwig", 5, 20, 20, 10, 5, 5);
-	Pokemon* chimchar = new Pokemon("Chimchar", 5, 20, 20, 10, 5, 5);
+	Game game;
 
-
-	turtwig->setupPokemon(turtwig, "pokemons.json", "moves.json");
-	chimchar->setupPokemon(chimchar, "pokemons.json", "moves.json");
-	me.addPokemon(turtwig);
-	rival.addPokemon(chimchar);
-
-	Battle battle(me, rival);
-
-	battle.battleLoop();
+	game.startGame();
 
 	return 0;
 
